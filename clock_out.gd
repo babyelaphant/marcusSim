6231c616876936c83d585ea8fddcdf8f6c798092
babyelaphant <amimutted@gmail.com>
2026-06-11 20:07:52 -0400

    adding a clock out button

--- clock_out.gd
+++ clock_out.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends Area2D
 
 var player_in_area = false
 @onready var player = $"../player"
@@ -11,15 +11,13 @@ func _ready():
 func _process(delta):
 	if player_in_area:
 		if Input.is_action_just_pressed("pickup"):
-			print("we pickedup the funko")
-			queue_free()
-			player.addFunkoPop()
-			# TODO make function to add one funko pop to the player
+			print("yoooo")
+			
 
-func _on_pickup_detection_body_entered(body):
+func _on_body_entered(body):
 	if body.has_method("player"):
 		player_in_area = true
 
-func _on_pickup_detection_body_exited(body):
+func _on_body_exited(body):
 	if body.has_method("player"):
 		player_in_area = false
